1772861a767350efb05dcea1ca3ccb10a57739f4
scotttyso <tyscott@cisco.com>
2022-09-30 23:47:23 -0400

    removing module check

--- main.tf
+++ main.tf
@@ -4,9 +4,10 @@ locals {
   intersight = lookup(var.model, "intersight", {})
   orgs       = var.orgs
   profiles   = lookup(local.intersight, "profiles", {})
-  domain_profiles = local.modules.profiles.domain == true ? {
+  domain_profiles = {
     for v in lookup(local.profiles, "domain", []) : v.name => {
       destroy                     = lookup(v, "destroy", local.defaults.intersight.profiles.domain.destroy)
+      domain_template             = lookup(v, "domain_template", "")
       fabric_switches             = ["A", "B"]
       name                        = v.name
       network_connectivity_policy = lookup(v, "network_connectivity_policy", "")
@@ -20,25 +21,26 @@ locals {
       vlan_policies               = lookup(v, "vlan_policies", [])
       vsan_policies               = lookup(v, "vsan_policies", [])
     }
-  } : {}
-  switch_profiles_loop = local.modules.profiles.domain == true ? flatten([
+  }
+  switch_profiles_loop = flatten([
     for v in local.domain_profiles : [
       for i in range(length(v.fabric_switches)) : {
-        destroy = v.destroy
-        # moid = v.destroy == false ? module.domain[
-        #   v.name]["domain_profile"][element(v.fabric_switches, i)
-        # ] : ""
+        destroy         = v.destroy
+        domain_template = v.domain_template
+        moid = v.destroy == false ? module.domain[
+          v.name]["domain_profile"][element(v.fabric_switches, i)
+        ] : ""
         name                        = "${v.name}-${element(v.fabric_switches, i)}"
-        network_connectivity_policy = lookup(v, "network_connectivity_policy", "")
-        ntp_policy                  = lookup(v, "ntp_policy", "")
+        network_connectivity_policy = v.network_connectivity_policy
+        ntp_policy                  = v.ntp_policy
         port_policy = length(v.port_policies) == 2 ? element(v.port_policies, i) : length(
           v.port_policies) == 1 ? element(v.port_policies, 0
         ) : ""
-        snmp_policy           = lookup(v, "snmp_policy", "")
+        snmp_policy           = v.snmp_policy
         serial_number         = length(v.serial_numbers) == 2 ? element(v.serial_numbers, i) : ""
-        switch_control_policy = lookup(v, "switch_control_policy", "")
-        syslog_policy         = lookup(v, "syslog_policy", "")
-        system_qos_policy     = lookup(v, "system_qos_policy", "")
+        switch_control_policy = v.switch_control_policy
+        syslog_policy         = v.syslog_policy
+        system_qos_policy     = v.system_qos_policy
         vlan_policy = length(v.vlan_policies) == 2 ? element(v.vlan_policies, i) : length(
           v.vlan_policies) == 1 ? element(v.vlan_policies, 0
         ) : ""
@@ -47,7 +49,7 @@ locals {
         ) : ""
       }
     ]
-  ]) : []
+  ])
   switch_profiles = { for v in local.switch_profiles_loop : v.name => v }
 }
 
@@ -58,18 +60,16 @@ locals {
 #____________________________________________________________
 
 module "domain" {
-  source  = "terraform-cisco-modules/profiles-domain/intersight"
-  version = ">= 1.0.5"
-  for_each = {
-    for dp in lookup(local.profiles, "domain", []) : dp.name => dp if lookup(local.modules.profiles, "domain", true)
-  }
-  action              = lookup(each.value, "action", local.defaults.intersight.profiles.domain.action)
-  destroy             = lookup(each.value, "destroy", local.defaults.intersight.profiles.domain.destroy)
-  description         = lookup(each.value, "description", "")
-  domain_src_template = lookup(each.value, "domain_src_template", "")
-  domain_type         = lookup(each.value, "domain_type", "instance")
-  name                = "${each.value.name}${local.defaults.intersight.profiles.domain.name_suffix}"
-  organization        = local.orgs[lookup(each.value, "organization", local.defaults.intersight.organization)]
-  serial_numbers      = lookup(each.value, "serial_numbers", [])
-  tags                = lookup(each.value, "tags", local.defaults.intersight.tags)
+  source          = "terraform-cisco-modules/profiles-domain/intersight"
+  version         = ">= 1.0.5"
+  for_each        = { for dp in lookup(local.profiles, "domain", []) : dp.name => dp }
+  action          = lookup(each.value, "action", local.defaults.intersight.profiles.domain.action)
+  destroy         = lookup(each.value, "destroy", local.defaults.intersight.profiles.domain.destroy)
+  description     = lookup(each.value, "description", "")
+  domain_template = lookup(each.value, "domain_template", "")
+  domain_type     = lookup(each.value, "domain_type", "instance")
+  name            = "${each.value.name}${local.defaults.intersight.profiles.domain.name_suffix}"
+  organization    = local.orgs[lookup(each.value, "organization", local.defaults.intersight.organization)]
+  serial_numbers  = lookup(each.value, "serial_numbers", [])
+  tags            = lookup(each.value, "tags", local.defaults.intersight.tags)
 }
